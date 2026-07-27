bd73122f9cf6929878ebd430f6d7496c4f3ff140
Krishan <krishan.sah@geekyants.com>
2026-07-27 15:18:50 +0530

    feat: update BlogPage component to display dynamic content and remove unused SVG files

--- my-app-01/app/blog/[test]/page.tsx
+++ my-app-01/app/blog/[test]/page.tsx
@@ -1,3 +1,18 @@
-export default function BlogPage() {
-    return "Blog";
+type Props = {
+  params: Promise<{
+    test: string;
+  }>
+}
+
+export default async function BlogPage({ params }: Props) {
+  const { test } = await params;
+
+  console.log(test);
+
+  return (
+    <>
+      <h1>Blog</h1>
+      <p>{test}</p>
+    </>
+  );
 }
