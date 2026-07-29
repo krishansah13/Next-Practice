abb571fb7057c7d4968c7e5dcc37910cad2c8935
Krishan <krishan.sah@geekyants.com>
2026-07-29 11:01:54 +0530

    feat: add styling and layout for MealSlug page; implement image and text display

--- meals-app/src/app/meals/[mealSlug]/page.tsx
+++ meals-app/src/app/meals/[mealSlug]/page.tsx
@@ -1,15 +1,58 @@
-type Props = {
-    params : Promise<{
-        mealSlug : string;
-    }>
-}
 
-const MealSlug = async({params} : Props) => {
-    const {mealSlug} = await params;
+import Image from "next/image";
+import classes from "./page.module.css"
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+const MealSlug = async ({ params }: {
+    params: {
+        mealSlug: string
+    }
+}) => {
+    const { mealSlug } = await params
+    const meal = getMeal(mealSlug) as {
+        image: string,
+        instructions: string,
+        title: string,
+        slug: string,
+        creator: string,
+        creator_email: string,
+        summary: string
+    };
+    if (!meal) {
+        notFound();
+    }
+    meal.instructions = meal.instructions.replace(/\n/g, '<br>');
+
     return (
-        <h1>
-            Meal {mealSlug}
-        </h1>
+        <>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.slug} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>
+                        {meal.title}
+                    </h1>
+                    <p className={classes.creator}>
+                        by &nbsp;
+                        <a href={`mailTo:${meal.creator_email}`}>
+                            {meal.creator}
+                        </a>
+                    </p>
+                    <p className={classes.summary}>
+                        {meal.summary}
+                    </p>
+                </div>
+            </header>
+            <main>
+                <p className={classes.instructions} dangerouslySetInnerHTML={
+                    { __html: meal.instructions, }
+                }>
+
+                </p>
+            </main>
+        </>
     )
 }
 
--- meals-app/src/lib/meals.ts
+++ meals-app/src/lib/meals.ts
@@ -6,3 +6,7 @@ export default async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   return db.prepare("SELECT * FROM meals").all();
 }
+
+export function getMeal(slug : string)  {
+    return db.prepare('SELECT * from meals where slug = ?').get(slug);
+}
