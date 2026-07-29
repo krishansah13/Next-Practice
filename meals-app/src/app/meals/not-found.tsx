9c0bf96a0a9747cc43cd70316949a38fa3a14bff
Krishan <krishan.sah@geekyants.com>
2026-07-29 11:19:03 +0530

    feat: add ShareMealPage component with form for meal sharing; create NotFound component; add styles for layout

---
+++ meals-app/src/app/meals/not-found.tsx
@@ -0,0 +1,14 @@
+const NotFound = () => {
+    return (
+        <main className="not-found">
+            <h1>
+                Not Found
+            </h1>
+            <p>
+                The requested meal cannot be found!
+            </p>
+        </main>
+    )
+}
+
+export default NotFound;
--- meals-app/src/app/meals/shares/page.tsx
+++
@@ -1,5 +0,0 @@
-const SharePage = () => {
-    return <h1>Share Page</h1>
-}
-
-export default SharePage;
