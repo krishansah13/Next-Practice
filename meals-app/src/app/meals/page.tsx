c57ac67f718ee243d326cdac4e8d8163d7765c4a
Krishan <krishan.sah@geekyants.com>
2026-07-29 10:16:44 +0530

    feat: implement error handling and loading state in MealsPage; add Error component and loading animations

--- meals-app/src/app/meals/page.tsx
+++ meals-app/src/app/meals/page.tsx
@@ -1,10 +1,18 @@
 import Link from "next/link"
+import { Suspense } from "react"
+
 import classes from "./page.module.css"
 import MealsGrid from "./meals-grid"
 import getMeals from "@/lib/meals"
 
-export default async function MealsPage() {
+
+
+async function Meals() {
     const meals = await getMeals();
+    return <MealsGrid meals={meals} />
+}
+
+export default function MealsPage() {
     return (
         <>
             <header className={classes.header}>
@@ -18,11 +26,17 @@ export default async function MealsPage() {
                     Choose your favorite recipe and cook it yourself. It is easy and amazing.
                 </p>
                 <p className={classes.cta}>
-                    <Link href = "/meals/share">Share Your Favorite Recipe</Link>
+                    <Link href="/meals/share">Share Your Favorite Recipe</Link>
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals}/>
+                <Suspense fallback={
+                    <p className={classes.loading}>
+                        Preparing the dishes in our backend...
+                    </p>
+                }>
+                    <Meals />
+                </Suspense>
             </main>
         </>
     )
