4755b86e5a11bdbe30654db0dc0cfe4400ac2b48
Krishan <krishan.sah@geekyants.com>
2026-07-31 10:54:43 +0530

    feat: add database connection utility and type definitions for mongoose

---
+++ video-app/lib/db.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI !
+
+if(!MONGODB_URI) {
+    throw new Error("Please Define the MONGODB_URI in the env variables");
+}
+
+let cached = global.mongoose;
+
+if(!cached) {
+    global.mongoose = {
+        conn : null,
+        promise : null
+    }
+}
+
+export async function connectToDatabase() {
+    if(cached.conn) {
+        return cached.conn;
+    } 
+    if(!cached.promise) {
+        mongoose.connect(MONGODB_URI)
+        .then(()=>mongoose.connection)
+    }
+    try {
+        cached.conn = await cached.promise;
+    }catch(error) {
+        cached.promise = null;
+        console.error(error);
+    }
+    return cached.conn;
+}
