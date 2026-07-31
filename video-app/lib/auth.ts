dad71f705f5cc3a66133a5803a33997e9e28890e
Krishan <krishan.sah@geekyants.com>
2026-07-31 12:20:04 +0530

    feat: implement credential-based authentication with error handling

--- video-app/lib/auth.ts
+++ video-app/lib/auth.ts
@@ -13,3 +13,52 @@ export const authOptions: NextAuthOptions = {
   ],
 };
 */
+
+import { NextAuthOptions } from "next-auth";
+import CredentialProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Missing email or password");
+        }
+
+        try {
+          await connectToDatabase();
+          const user = await User.findOne({ email: credentials.email });
+          if (!user) {
+            throw new Error("No User Found with this");
+          }
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password,
+          );
+
+          if (!isValid) {
+            throw new Error("Invalid Password");
+          }
+          return {
+            id : user._id.toString(),
+            email : user.email
+          }
+        } catch (error) {
+            console.error("Auth error " , error);
+            throw error
+        }
+      },
+    }),
+  ],
+};
