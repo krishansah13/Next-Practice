57f301a0a348fc67c4fa0149c926e8d489158ffd
Krishan <krishan.sah@geekyants.com>
2026-07-31 11:33:36 +0530

    feat: implement user registration API and extend NextAuth session type

---
+++ video-app/app/api/auth/register/route.ts
@@ -0,0 +1,51 @@
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password }  = await request.json();
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 },
+      );
+    }
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        {
+          error: "User Already Registered",
+        },
+        { status: 400 },
+      );
+    }
+
+    await User.create({ email, password });
+
+    return NextResponse.json(
+      { message: "User registered successfully" },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error(`Registration error :  ${error}`);
+    return NextResponse.json(
+      {
+        error: "User registration failed",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+}
+
+/* get Data from frontEnd
+ * Validation
+ * 1. existing user check
+ * 2. create user in DB
+ * 3. return success response
+ */
